b68e09b7a72b0d84b780124952a0c20c0916718f
derekharget <git@derekharget.com>
2021-09-20 17:24:23 -0400

    Copied Login Page to Register

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,33 +1,83 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Box from "@mui/material/Box";
-import {TextField} from "@mui/material";
+import {CircularProgress, TextField, Typography} from "@mui/material";
+import Button from "@mui/material/Button";
+import {useForm} from "react-hook-form";
+import {useMutation, useQueryClient} from "react-query";
+import Auth from '../../hooks/Auth/Auth';
 
-const Register = () => {
+
+const Login = () => {
+    const {register, handleSubmit} = useForm();
+    const [isError, setIsError] = useState(false);
+
+    const queryClient = useQueryClient();
+
+    const {mutate, isLoading} = useMutation(Auth.login, {
+        onSuccess: data => {
+            console.log(data);
+            setIsError(false);
+        },
+        onError: () => {
+            setIsError(true);
+        },
+        onSettled: () => {
+            queryClient.invalidateQueries('create');
+        }
+    });
+
+
+    const onSubmit = data => {
+        mutate(data);
+        console.log(data);
+    }
+
+
     return (
-        <Box sx={{width: '100%', bgcolor: '#cfe8fc', }}>
-            <TextField
-                required
-                id="outlined-required"
-                label="Username"
-            />
+        <Box sx={{textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%'}}>
+            <Typography variant="h4">Login</Typography>
 
-            <TextField
-                required
-                id="email"
-                label="Email"
-                type="email"
-            />
+            <Box sx={{maxWidth: 'md', bgcolor: '#cfe8fc', mt: 4}} component="form"
+                 onSubmit={handleSubmit(onSubmit)}>
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    {...register("email")}
+                    label="Email"
+                    autoComplete="off"
+                />
 
 
-            <TextField
-                required
-                id="outlined-password-input"
-                label="Password"
-                type="password"
-                autoComplete="current-password"
-            />
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    {...register("password")}
+                    autoComplete="current-password"
+                />
+                {isError && <Typography variant="subtitle2" sx={{ color: 'red'}}>Error Occurred Processing Your Request</Typography>}
+
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{mt: 3, mb: 2, maxWidth: {sm: '100%', md: '50%'}}}
+                >
+                    {!isLoading && (`Sign In`)}
+                    {isLoading && (
+                        <CircularProgress
+                            sx={{color: 'white', alignItems: 'left'}}
+                            size={20}
+                        />
+                    )}
+                </Button>
+            </Box>
         </Box>
     );
 };
 
-export default Register;
+export default Login;
